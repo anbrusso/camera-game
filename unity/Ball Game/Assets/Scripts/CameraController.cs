46157ab559d522f8f34c227e0b0aec21d77d9657
Andrew Brusso <albrusso@mtu.edu>
2020-04-09 00:22:15 -0400

    Initial commit, some of the game is done, basic face detection is sort of there.

---
+++ unity/Ball Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(player.transform.position);
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame -- garaunteed to run after all items (e.g. player has moved)
+    void LateUpdate()
+    {
+        float rotHorizontal = Input.GetAxis("Horizontal");
+        PlayerController pc = player.GetComponent<PlayerController>();
+        Vector3 rotateVal = new Vector3(0,0,pc.rotSpeed * rotHorizontal);
+        if ((transform.eulerAngles.z + rotateVal.z) <= 315 && transform.eulerAngles.z >= 315)
+        {
+            //rotateVal = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 315);
+            //transform.eulerAngles = rotateVal;
+            //Debug.Log("Right Camera Limit");
+        }
+        else if ((transform.eulerAngles.z + rotateVal.z) >= 45 && transform.eulerAngles.z <= 45)
+        {
+            //rotateVal = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 45);
+            //transform.eulerAngles = rotateVal;
+            //Debug.Log("Left Camera Limit");
+        }
+        else
+        {
+            transform.eulerAngles = transform.eulerAngles + rotateVal;
+        }
+        //Debug.Log(transform.eulerAngles.z);
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ unity/Ball Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotSpeed;
+    public float gravitySpeed;
+    private Vector3 gravityDir;
+    private Rigidbody rb;
+    private Vector3 oldVelocity;
+    private bool stopped = false;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gravityDir = new Vector3(0, 0, -1);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !stopped)
+        {
+            oldVelocity = rb.velocity;
+            rb.velocity = Vector3.zero;
+            stopped = true;
+            Debug.Log("Down, not stopped");
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && stopped)
+        {
+            rb.velocity = oldVelocity;
+            oldVelocity = Vector3.zero;
+            stopped = false;
+            Debug.Log("Up, stopped");
+        }
+    }
+    //physics related code
+    private void FixedUpdate()
+    {
+
+        float rotHorizontal = Input.GetAxis("Horizontal");
+        if (gravityDir.x > .5 && rotHorizontal > 0)
+        {
+            //Debug.Log("Left Limit Player");
+        }
+        else if (gravityDir.x < -.5 && rotHorizontal < 0)
+        {
+            //Debug.Log("Right Limit Player");
+        }
+        else
+        {
+            gravityDir = Quaternion.AngleAxis(-rotSpeed * rotHorizontal, Vector3.up) * gravityDir;
+        }
+
+        if (!stopped) {
+            rb.AddForce(gravitySpeed * gravityDir);
+        }
+    }
+}
