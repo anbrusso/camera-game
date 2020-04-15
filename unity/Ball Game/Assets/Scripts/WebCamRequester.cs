aab97eb372762de895b9419baf463d79557768aa
Andrew Brusso <albrusso@mtu.edu>
2020-04-15 00:26:40 -0400

    After the worlds biggest headache, Unity now starts and stops the camera python code! Works pretty slick, going to need to add messages for when the camera is connected.

--- unity/Ball Game/Assets/Scripts/WebCamRequester.cs
+++ unity/Ball Game/Assets/Scripts/WebCamRequester.cs
@@ -9,65 +9,44 @@ public class WebCamRequester : RunAbleThread
     static TimeSpan timout = TimeSpan.FromMilliseconds(1000);
     public float headAngle = 0;
     public bool connected;
-    private float timer = 0;
-    private bool reconnectTimeout = false;
     protected override void Run()
     {
         while (Running)
         {   
-            //if connection failed, wait 5 seconds before reconnecting.
-           /* if (reconnectTimeout)
+            //try connecting to the server
+            connected = false;
+            try
             {
-                Debug.Log("Reconnect Timout....");
-                if (timer < 5)
+                ForceDotNet.Force();
+                using (RequestSocket client = new RequestSocket())
                 {
-                    timer += Time.deltaTime;
-                }
-                else
-                {
-                    reconnectTimeout = false;
-                    timer = 0;
-                    Debug.Log("Reconnect Timout lapsed, Retrying");
-                }
-            }
-            else
-            {*/
-                //try connecting to the server
-                connected = false;
-                try
-                {
-                    ForceDotNet.Force();
-                    using (RequestSocket client = new RequestSocket())
+                    string angle = null;
+                    client.Connect("tcp://localhost:5555");//connect to server
+                                                            //repeatedly send/receive packets to the server to get the angle.
+                    while (Running)
                     {
-                        string angle = null;
-                        client.Connect("tcp://localhost:5555");//connect to server
-                                                               //repeatedly send/receive packets to the server to get the angle.
-                        while (Running)
+                        //ask for an angle. If we can't send/receive for over a second, then assume that the server went down.
+                        if (client.TrySendFrame("a") && client.TryReceiveFrameString(timout, out angle) && Running)
                         {
-                            //ask for an angle. If we can't send/receive for over a second, then assume that the server went down.
-                            if (client.TrySendFrame("a") && client.TryReceiveFrameString(timout, out angle) && Running)
-                            {
-                                headAngle = float.Parse(angle);
-                                connected = true;
-                                Debug.Log("Connected");
-                            }
-                            else
-                            {
-                                connected = false;
-                                Debug.Log("Not connected");
-                            }
+                            headAngle = float.Parse(angle);
+                            connected = true;
+                            //Debug.Log("Connected");
+                        }
+                        else
+                        {
+                            connected = false;
+                            //Debug.Log("Disconnected");
                         }
                     }
-                    NetMQConfig.Cleanup();
                 }
-                catch (Exception e)
-                {
-                    reconnectTimeout = true;
-                    NetMQConfig.Cleanup();
-                    Debug.Log("Connection Failed.");
-                    //Debug.Log(e.StackTrace);
-                }
-            //}
+                NetMQConfig.Cleanup();
+            }
+            catch (Exception e)
+            {
+                NetMQConfig.Cleanup();
+                //Debug.Log("Connection Failed.");
+                //Debug.Log(e.StackTrace);
+            }
         }
     }
 }
